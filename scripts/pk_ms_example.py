26850acf409d90ccebf59f1ab38c5eb8b93a4ddb
Charles Snider <charlessnider61@gmail.com>
2024-03-25 13:26:40 -0400

    Started QC items

--- scripts/pk_ms_example.py
+++ scripts/pk_ms_example.py
@@ -8,7 +8,7 @@ from bbc.xfutils import *
 xf_env = {
     "variables": {
         # Hardcoding for the example. In a larger loader process, this will be pulled from a meta-data sheet.
-        "sample_id": "1618289",
+        "sample_id": "1618423",
 
         # Tables
         "ms_table": {
@@ -49,6 +49,29 @@ xf_env = {
 
         # STD replicates are not directly labeled, use row offsets
         "std_replicate_offsets": [0, 1, 2],
+
+        # qc values
+        "qc_labels": ["ULQC", "LQC", "MQC", "HQC"],
+
+        # qc concs
+        "qc_conc_values": [3, 30, 300, 3000],
+
+        # zip together
+        "qc_labels_conc_values": {
+            "requires": ["qc_labels", "qc_conc_values"],
+            "type": zip,
+            "params": [Rf("qc_labels"), Rf("qc_conc_values")],
+            "xf": list,
+            # "debug": print,
+        },
+
+        # let's keep everything in the env-- we are assuming these are always the same, & come from the machine
+        "qc_name_map": {
+            "ULQC": "QC1",
+            "LQC": "QC2",
+            "MQC": "QC3",
+            "HQC": "QC4",
+        },
 
     }
 }
@@ -125,11 +148,93 @@ std_replicate_sheet_def = {
 }
 
 
+qc_replicate_sheet_def = {
+    "sheet_name": "QC Calibration",
+
+    "row_iterator": [
+        (Rf("qc_labels_conc_values"), "qc_label_conc"),
+    ],
+
+    "row_env_update": DictUpdate(OrderedDict((
+        ("qc_label", lambda env: env["qc_label_conc"][0]),
+        ("qc_conc", lambda env: env["qc_label_conc"][1]),
+    ))),
+
+    "columns": [
+        {
+            "header": "Sample Name",
+            "xf": EnvDict("qc_name_map", Rf("qc_label"))
+        },
+        {
+            "header": "Nominal Concentration (nM)",
+            "xf": Rf("qc_conc")
+        },
+        {
+            "header": "1 - Calculated Concentration (nM)",
+            "xf": TableValue(
+                Rf("ms_table"),
+                Rf("qc_label", "qc_conc"),
+                "Calculated Conc",
+                cmp=b_starts_with_a,  # if we give you ULQC, and you see ULQC_#, give us that row
+            )
+        },
+        {
+            "header": "1 - Accuracy (%)",
+            "xf": TableValue(
+                Rf("ms_table"),
+                Rf("qc_label", "qc_conc"),
+                "Calculated Conc",
+                cmp=b_starts_with_a,
+                row_offset=0,
+                xf_env=lambda v, env: float(v) / float(env["qc_conc"])  # accuracy as a fraction
+            ),
+        },
+        {
+            "header": "3 - Calculated Concentration (nM)",
+            "xf": TableValue(
+                Rf("ms_table"),
+                Rf("qc_label", "qc_conc"),
+                "Calculated Conc",
+                cmp=b_starts_with_a,  # if we give you ULQC, and you see ULQC_#, give us that row
+                row_offset=2,
+            )
+        },
+        {
+            "header": "3 - Accuracy (%)",
+            "xf": ""
+        },
+        {
+            "header": "5 - Calculated Concentration (nM)",
+            "xf": TableValue(
+                Rf("ms_table"),
+                Rf("qc_label", "qc_conc"),
+                "Calculated Conc",
+                cmp=b_starts_with_a,  # if we give you ULQC, and you see ULQC_#, give us that row
+                row_offset=4,
+            )
+        },
+        {
+            "header": "5 - Accuracy (%)",
+            "xf": ""
+        },
+        {
+            "header": "7 - Calculated Concentration (nM)",
+            "xf": ""
+        },
+        {
+            "header": "7 - Accuracy (%)",
+            "xf": ""
+        },
+    ]
+}
+
+
 class MSExtractor(XFProcessor):
     xf_env = xf_env
 
     sheet_defs = [
         std_replicate_sheet_def,
+        qc_replicate_sheet_def
     ]
 
 if __name__=="__main__":
